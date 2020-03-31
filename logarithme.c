f31019f0d4ecde3f53d3e1f6886376e6e0c29603
Meysonnet2 <frederic.meysonnet@univ-st-etienne.fr>
2020-03-31 22:37:53 +0200

    j'ai fait loga

--- logarithme.c
+++ logarithme.c
@@ -1,14 +1,1 @@
-resolu<<<<<<< HEAD
-
-sur master2
-=======
-sur devmaster2
->>>>>>> origin/devmaster2
-travaille sur dev par Meysonnet2
-modif par Meysonnet 2 sMODIF
-
-<<<<<<< HEAD
-Hello from master branch 
-=======
-Hello from log-dev branch 
->>>>>>> log-dev
+log sur machine 2
